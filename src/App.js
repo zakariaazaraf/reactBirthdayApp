d53de3947b421a39102be29d8cf557d6e9b4228b
zakaria ben salek <zakariaazaraf@gmail.com>
2021-02-15 22:31:48 +0100

    complate the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from "react"
+
 import './App.css';
+import data from "./data"
+import { useState } from 'react';
+
+console.log(data.length)
 
 function App() {
+  const [birthdays, setBirthdays] = useState(data)
+  const clear = () =>{
+    setBirthdays([])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3>{birthdays.length} Birthdays Today</h3>
+      {birthdays.map(date =>{
+        const {image, id, name, age} = date
+        return (
+          <div key={id} className="info">
+            <img src={image} alt={name}></img>
+            <div>
+              <p className="name">{name}</p>
+              <p>{age} Years</p>
+            </div>
+            
+          </div>
+        )       
+      })}
+      <button onClick={clear}>Clear All</button>
     </div>
   );
 }
